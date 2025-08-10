1522d69c55f06d77e3cc1ecd131e07f0a57532f3
Sachin Bali <sachinbali228@gmail.com>
2025-08-10 16:24:45 +0530

    Added : StringFilterAdvanceExample

---
+++ src/main/java/StringFilterAdvanceExample.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+@FunctionalInterface
+interface FilterString{
+    boolean isRequiredIncluded(String  str);
+}
+
+public class StringFilterAdvanceExample {
+    public static void main(String[] args) {
+        String[] fruitStringArray = {"Apple","Banana", "SweetPotato","Mango", "strawberry"};
+        System.out.println("All Fruits Start from 'S' or 's' Are : ");
+        FilterString filterString = str -> str.startsWith("S") || str.startsWith("s");
+        Arrays.stream(filterStringArray(fruitStringArray, filterString)).forEach(System.out::println);
+    }
+
+    private static String[] filterStringArray(String[] stringArray, FilterString filterString){
+        return Arrays.stream(stringArray)
+                .filter(filterString::isRequiredIncluded)
+                .toList().toArray(new String[0]);
+    }
+}
+
